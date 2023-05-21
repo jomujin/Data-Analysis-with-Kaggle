88656f1460d0030c113eaabf646b2ac32a02ec57
jomujin <jomujin@gmail.com>
2023-05-21 22:25:08 +0900

    NbaBoxScore class 추가하고 Nba class를 상속받도록 하라

--- nba_stats/Nba.py
+++ nba_stats/Nba.py
@@ -134,3 +134,33 @@ class Nba:
                     json_data = json.loads(json_str)
                     self.jsons.append(json_data)
 
+
+class NbaBoxScore(Nba):
+    def __init__(
+        self,
+        idx
+    ):
+        super().__init__(self, idx)
+
+    def _get_url(self, page_typ: str):
+        return super()._get_url(page_typ)
+
+    def _get_req(self):
+        return super()._get_req()
+
+    def _get_html(self):
+        return super()._get_html()
+
+    def _get_find_by_tag(self, tag: str, type_var: str):
+        return super()._get_find_by_tag(tag, type_var)
+
+    def _get_json_by_compile(self, compile: str, flags: str = re.DOTALL):
+        return super()._get_json_by_compile(compile, flags)
+
+    self._get_url(
+        page_typ='boxscore'
+    )
+    self._get_find_by_tag(
+        tag='script',
+        type_var='text/javascript'
+    )
